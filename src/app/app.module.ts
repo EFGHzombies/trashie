698d2a4ec94f1a92fd8bf78c2ed042013cde61e3
Matthew Wyatt <mattwyatt3@gmail.com>
2019-01-16 22:13:39 -0800

    floors commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FloorsComponent } from './floors/floors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FloorsComponent
   ],
   imports: [
     BrowserModule
