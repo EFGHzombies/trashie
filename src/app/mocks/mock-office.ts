d390454b92b500c1df61cfe79e30176058af2140
Matthew Wyatt <mattwyatt3@gmail.com>
2019-02-04 16:50:43 -0800

    added bin pic files and bins in mock

--- src/app/mocks/mock-office.ts
+++ src/app/mocks/mock-office.ts
@@ -4,7 +4,7 @@ import { F_TWO } from '../mocks/mock-floorTwo';
 import { B_ONE } from '../mocks/mock-baseOne';
 
 export const OFFICE: FloorInterface[] = [
-    { id: 0, floorLevel: '2', restRoom: true, food: false, bins: F_ONE, floorStatus: true },
-    { id: 1, floorLevel: '1', restRoom: true, food: true, bins: F_TWO, floorStatus: true },
+    { id: 0, floorLevel: '2', restRoom: true, food: false, bins: F_TWO, floorStatus: true },
+    { id: 1, floorLevel: '1', restRoom: true, food: true, bins: F_ONE, floorStatus: true },
     { id: 2, floorLevel: 'B1', restRoom: true, food: true, bins: B_ONE, floorStatus: true }
 ];
