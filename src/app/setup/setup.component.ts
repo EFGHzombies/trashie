4836d4fb4f7ab642a4a433fd1cd5094159acb7ec
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-03-06 21:03:15 -0800

    For loop closure conflicts

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -11,6 +11,7 @@ import { TRASHarray } from '../mocks/default-trash';
 import { TRASHbin } from '../mocks/default-trash';
 
 import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-setup',
@@ -27,19 +28,57 @@ export class SetupComponent implements OnInit {
   private floorIn: FloorInterface = DEFAULTfloor;
   
   private binCount: string;
+
+  private a = {
+    id: 'a',
+    name: 'test',
+    num: 1
+  };
+  private x = {
+    head: 'hello',
+    list: []
+  };
   
   constructor(private authService: AuthService, private setupService: SetupService, private router: Router) { }
 
   addFloor() {
     this.floorIn.id = this.idCount;
     this.idCount = this.idCount + 1;
-    for(let i = parseInt(this.binCount) - 1; i > 0; i--) {
+    for(let i = parseInt(this.binCount)-1; i > 0; i--) {
       this.floorIn.bins.push(TRASHbin);
     }
-    for(let i = 1; i < this.floorIn.bins.length; i++) {
-      this.floorIn.bins[i].id = String.fromCharCode(this.floorIn.bins[i].id.charCodeAt(0) + i);
-    }
-    console.log(this.floorIn);
+    // for(let i = 1; i < this.floorIn.bins.length; i++) {
+    //   this.floorIn.bins[i].id = String.fromCharCode(this.floorIn.bins[i].id.charCodeAt(0) + i);
+    // }
+
+    // console.log(this.floorIn);
+    this.building.floor.push(this.floorIn);
+    console.log(this.building);
+    
+  }
+
+  test() {
+    this.x.list.push(this.a);
+    this.x.list.push(this.a);
+    this.x.list.push(this.a);
+
+    // let i = 0;
+    // this.x.list.forEach(element => {
+    //   this.getId(i);
+    //   i++;
+    // });
+
+    // for(let i = 0; i<this.x.list.length; i++) {
+    //   this.getId(i);
+    //   console.log(this.x.list[i]);
+    // }
+    
+    console.log(this.x.list);
+  }
+
+  getId(z: number) {
+    let a = 'a';
+    this.x.list[z].id = String.fromCharCode(a.charCodeAt(0) + z);
   }
 
   isLoggedIn() {
